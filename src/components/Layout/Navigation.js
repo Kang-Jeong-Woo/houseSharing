7e248cae7e245ece4926d410cc18df2d2ab74f54
jeongwoo-kang <zzangkbc1@gmail.com>
2023-01-17 18:14:35 +0900

    [업데이트] navigation pop-up 작업중

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -1,10 +1,15 @@
 import classes from "./Navigation.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
+import Sidebar from "./Sidebar";
 const Navigation = props => {
+    const [menuOn, setMenuOn] = useState(true);
+
     return(
         <div className={classes.navigation}>
             <div className={classes.wrapper}>
+                {menuOn && <Sidebar/>}
                 <div><FontAwesomeIcon icon={faBars} className={classes.menu}/></div>
                 <div>
                     <span>사용자이름</span>
