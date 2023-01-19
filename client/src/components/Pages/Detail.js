7e8c2b219fe2b6745708f1e2c049a25641eb7fff
jeongwoo-kang <zzangkbc1@gmail.com>
2023-01-19 16:24:32 +0900

    [업데이트] antd 를 이용한 detail 페이지 작업중...(30%)

--- client/src/components/Pages/Detail.js
+++ client/src/components/Pages/Detail.js
@@ -6,12 +6,14 @@ import Page from "../UI/Page";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
 import {far} from "@fortawesome/free-regular-svg-icons";
+import {DatePicker} from "antd";
 
 
 const Detail = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedHouse, setLoadedHouse] = useState([]);
     const location = useLocation();
+    const {RangePicker} = DatePicker;
 
     useEffect(() => {
         setIsLoading(true);
@@ -32,34 +34,31 @@ const Detail = () => {
         <Page>
             <div className={classes.totalCntnr}>
                 <div className={classes.titleCntnr}>
-                    <div>{loadedHouse.name}</div>
-                    <div>{loadedHouse.address}</div>
+                    <div className={classes.title}>{loadedHouse.name}</div>
+                    <div >{loadedHouse.address}</div>
                     {/*<FontAwesomeIcon icon={[far , Heart]}/>*/}
                 </div>
                 <div className={classes.imgCntnr}>
                     <img src={loadedHouse.img} alt={loadedHouse.name}/>
                 </div>
                 <div className={classes.descCntnr}>
-                    <div className={classes.desc}></div>
-                    <div className={classes.payment}></div>
-                </div>ㅏ
+                    <div className={classes.desc}>
+
+                    </div>
+                    <div className={classes.payment}>
+                        요금을 확인하려면 날짜를 입력하세요
+                        <div className={classes.block}>
+                            <RangePicker bordered={false}/>
+                        </div>
+                        <input type={"number"} name={"human"} min={1} max={5}/>
+                    </div>
+
+                </div>
                 <div className={classes.reviewCntnr}>{"여기가 후기 section"}</div>
                 <div className={classes.locationCntnr}>{"여기가 카카오 지도"}</div>
                 <div className={classes.hostCntnr}>{"여기가 집주인 소통 창구"}</div>
             </div>
 
-
-
-
-
-            <div className={classes.container}>
-                <img key={loadedHouse.id} src={loadedHouse.img} alt={loadedHouse.name}/>
-                <div className={classes.content}>
-                    <div className={classes.title}>{loadedHouse.name}</div>
-                    <div className={classes.comment}>{loadedHouse.comment}</div>
-                    <div className={classes.hashtag}>{loadedHouse.hashtag}</div>
-                </div>
-            </div>
         </Page>
     )
 }
